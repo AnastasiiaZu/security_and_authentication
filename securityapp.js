2dac1029bad327d6aa4c0c37ae23388e0e42f4e4
Anastasiia Zubkova <anastasiia.zubkova.pro@gmail.com>
2021-05-07 10:08:00 +0200

    Salting and hashing with bcrypt

--- securityapp.js
+++ securityapp.js
@@ -4,13 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5"); //hashing function for passwords
+//installing a new hashing methind and defining number of salt-encryption rounds
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
@@ -19,77 +23,83 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 
 //mongo Schema and model
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
 app.route("/login")
 
-.get(function(req, res){
-  res.render("login");
-})
+  .get(function(req, res) {
+    res.render("login");
+  })
 
-.post(function(req, res){
-  const username = req.body.username;
-  const password = md5(req.body.password); // using the md5 hashing function on the password field
+  .post(function(req, res) {
+      const username = req.body.username;
+      const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if(err){
-      console.log(err);
-    }else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }else{
-          res.send("Wrong password!");
+      User.findOne({
+          email: username
+        }, function(err, foundUser) {
+          if (err) {
+            console.log(err);
+          } else {
+            if (foundUser) {
+              bcrypt.compare(password, foundUser.password, function(err, result) {
+                if (result === true) {
+                  res.render("secrets");
+                } else {
+                  res.send("Wrong password!");
+                };
+              });
+          } else {
+            res.send("Wrong username!");
+          }
         }
-      }else{
-        res.send("Wrong username!");
-      }
-    }
+      });
   });
-});
 
 
 
 ////////////////////////REGISTER PAGE///////////////////////
 app.route("/register")
 
-.get(function(req, res){
-  res.render("register");
-})
+  .get(function(req, res) {
+    res.render("register");
+  })
 
-.post(function(req, res){
+  .post(function(req, res) {
+    //using bcrypt hash and 10 rounds of salting to generate a crypted password from a plain-text password
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
 
- const newUser = new User ({
-   email: req.body.username,
-   password: md5(req.body.password) // using the md5 hashing function on the password field
- });
-
- newUser.save(function(err){
-   if (err){
-     console.log(err);
-   }else{
-     res.render("secrets");
-   };
- });
-});
-
-
-
-
+      newUser.save(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        };
+      });
+    });
+  });
 
 
 
 
 
-app.listen(3000, function(){
+
+
+
+
+app.listen(3000, function() {
   console.log("This server is running on port 3000!")
 });
