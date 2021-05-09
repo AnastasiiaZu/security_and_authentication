e16174b9717c79224c9d1a02967d066adb9e7c32
Anastasiia Zubkova <anastasiia.zubkova.pro@gmail.com>
2021-05-09 18:08:08 +0200

    Version with implemented google outhentication

--- securityapp.js
+++ securityapp.js
@@ -1,12 +1,11 @@
 //////////////////requiring and using all npm modules////////////////
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//installing a new hashing methind and defining number of salt-encryption rounds
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose"); //don't need to require passport-local
 
 const app = express();
 
@@ -16,10 +15,21 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Any long string in here.",//save to .env file
+  resave: false,
+  saveUninitialized: false
+}));
+
+//starting and using passport package on express-sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+mongoose.set('useCreateIndex', true);
 
 //mongo Schema and model
 
@@ -28,12 +38,21 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose); //to hash and salt passwords behind scenes
+
 const User = new mongoose.model("User", userSchema);
+
+//these will create cookie sessions and read throught them with passport-local-mongoose package
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
+/////////////////////////////////////LOGIN PAGE/////////////////////////////////////
 app.route("/login")
 
   .get(function(req, res) {
@@ -41,33 +60,26 @@ app.route("/login")
   })
 
   .post(function(req, res) {
-      const username = req.body.username;
-      const password = req.body.password;
 
-      User.findOne({
-          email: username
-        }, function(err, foundUser) {
-          if (err) {
-            console.log(err);
-          } else {
-            if (foundUser) {
-              bcrypt.compare(password, foundUser.password, function(err, result) {
-                if (result === true) {
-                  res.render("secrets");
-                } else {
-                  res.send("Wrong password!");
-                };
-              });
-          } else {
-            res.send("Wrong username!");
-          }
-        }
+    const user = new User ({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+  req.login(user, function(err){
+    if(err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
       });
+    }
+   });
   });
 
 
 
-////////////////////////REGISTER PAGE///////////////////////
+////////////////////////////////////REGISTER PAGE////////////////////////////////
 app.route("/register")
 
   .get(function(req, res) {
@@ -75,23 +87,36 @@ app.route("/register")
   })
 
   .post(function(req, res) {
-    //using bcrypt hash and 10 rounds of salting to generate a crypted password from a plain-text password
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      const newUser = new User({
-        email: req.body.username,
-        password: hash
-      });
 
-      newUser.save(function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render("secrets");
-        };
-      });
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        res.redirect("/register")
+      } else {
+        //authenticating and setting up a logged-in session for the user
+        passport.authenticate("local")(req, res, function(){
+          res.redirect("/secrets");
+        });
+      }
     });
   });
 
+/////////////////////////////////////LOGOUT ROUTE/////////////////////////////////////
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+/////////////////////////////////////SECRETS PAGE/////////////////////////////////////
+app.get("/secrets", function(req, res){
+  //checking whether the user is authenticated (passport-local-mongoose package)
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
 
 
